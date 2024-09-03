5d00fd4b82d4b62689d672afe3b83f70db283f6a
Camilo Olivares <140129147+CamilOlivares@users.noreply.github.com>
2024-09-02 22:12:43 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html> 
 <html lang="es">
 <head>
+ <body>
     <meta charset="UTF-8"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Titulo</title> 
@@ -17,5 +18,5 @@
     <script src="js/cursor.js"></script>
   
 
-
+</body>
 </html>
