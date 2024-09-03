4fe4818cfd906d893f5e18acadc0c3e2d36cfdca
Camilo Olivares <140129147+CamilOlivares@users.noreply.github.com>
2024-09-02 22:11:55 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
-<!DOCTYPE html> <!-- Define el tipo de documento como HTML5 -->
-<html lang="es"> <!-- Indica que el idioma del contenido es español -->
+<!DOCTYPE html> 
+<html lang="es">
 <head>
-    <meta charset="UTF-8"> <!-- Define la codificación de caracteres como UTF-8 -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Configura el viewport para un diseño responsivo -->
-    <title>Mi Portafolio</title> <!-- Título de la página que aparece en la pestaña del navegador -->
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Titulo</title> 
+   
     <!-- Enlaces a hojas de estilo CSS -->
-
-    <link rel="stylesheet" href="assets/css/cursor.css"> <!-- Estilos personalizados para el cursor (ajustar segun donde se ubique css-->
+    <link rel="stylesheet" href="assets/css/cursor.css"> <!-- Estilos personalizados para el cursor (ajustar segun donde se ubique css)-->
   
   <!--Contendio de la Página -->
 
 
     <!-- JavaScript de AOS para efectos de animación al hacer scroll con el cursor -->
     <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script> 
-    <!-- Scripts personalizados para el cursor -->
+    <!-- Scripts personalizados para el cursor (ajustar dependiendo de donde se tenga el script js)-->
     <script src="js/cursor.js"></script>
   
 
