058b996c8bbf172f7bc3f181865e5a9e30c100d7
Camilo Olivares <140129147+CamilOlivares@users.noreply.github.com>
2024-09-02 22:09:59 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html> <!-- Define el tipo de documento como HTML5 -->
+<html lang="es"> <!-- Indica que el idioma del contenido es español -->
+<head>
+    <meta charset="UTF-8"> <!-- Define la codificación de caracteres como UTF-8 -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- Configura el viewport para un diseño responsivo -->
+    <title>Mi Portafolio</title> <!-- Título de la página que aparece en la pestaña del navegador -->
+    <!-- Enlaces a hojas de estilo CSS -->
+
+    <link rel="stylesheet" href="assets/css/cursor.css"> <!-- Estilos personalizados para el cursor (ajustar segun donde se ubique css-->
+  
+  <!--Contendio de la Página -->
+
+
+    <!-- JavaScript de AOS para efectos de animación al hacer scroll con el cursor -->
+    <script src="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.js"></script> 
+    <!-- Scripts personalizados para el cursor -->
+    <script src="js/cursor.js"></script>
+  
+
+</body>
+</html>
